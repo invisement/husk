c392dd6b4ff22ac18833db952f39391d1d80c655
khosro <ali.khosro@slalom.com>
2024-10-24 03:30:55 -0700

    documentation

--- mod.ts
+++ mod.ts
@@ -1,4 +1,12 @@
-/** A tiny framework that provides helpers to build a full stack web application in typescript and deno */
+/**
+ * A tiny framework that provides helpers to build a full stack web application in typescript and deno
+ * Although it is tiny but it aims to make you develop a full stack app without using any ui or backend framework.
+ * You just need to use native server that Deno or Bun provides.
+ * It has utilities to transpile, bundle, and watch for ui development.
+ * It has pub-sub and event-waiter for state management (especially ui).
+ * It offers some basic web components.
+ * For backend, it has a router.
+ */
 
 export { Router } from "./server/router.ts";
 export type { Options, Route } from "./server/router.ts";
